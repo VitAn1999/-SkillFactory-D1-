3bf7ad06fb42f02e643aa978ec0cbd1ea83df4b2
Рощупкин Виталий <elpistolero1999@gmail.com>
2021-07-05 22:15:10 +0300

    add def move()

--- trello_client.py
+++ trello_client.py
@@ -20,6 +20,7 @@ def read():
         
 def create(name, column_name):
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+
     for index, column in enumerate(column_data):
         if column['name'] == column_name:
             post_card = requests.post(base_url.format('cards'), {'name': name, 'idList': column['id'], **auth_params})
@@ -29,7 +30,32 @@ def create(name, column_name):
                 post_card.raise_for_status()
             break
         elif index == len(column_data) - 1:
-            print(len('Такой колонки не существует, проверьте правильность введенных данных'))
+            print('Такой колонки не существует, проверьте правильность введенных данных')
+
+
+def move(name, column_name):
+    column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
+    task_id = None
+
+    for column in column_data:
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()
+        for task in column_tasks:
+            if task['name'] == name:
+                task_id = task['id']
+                break
+        if task_id:
+            break
+
+    for index, column in enumerate(column_data):
+        if column['name'] == column_name:
+            put_card = requests.put(base_url.format('cards') + '/' + task_id + '/idList', data={'value': column['id'], **auth_params})
+            if put_card.status_code == requests.codes.ok:
+                print(f'Задача {name} успешно перенесена в {column_name}')
+            else:
+                put_card.raise_for_status()
+            break
+        elif index == len(column_data) - 1:
+            print('Такой колонки не существует, проверьте правильность введенных данных')
 
 
 if __name__ == "__main__":
@@ -37,5 +63,7 @@ if __name__ == "__main__":
         read()
     elif sys.argv[1] == 'create':
         create(sys.argv[2], sys.argv[3])
+    elif sys.argv[1] == 'move':
+        move(sys.argv[2], sys.argv[3])
     else:
         print('Проверьте правильность введенной команды')
